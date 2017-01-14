6ea535c16ea5296be7c88bb7e2cb44149097dd61
penglu <penglu@360.cn>
2017-01-14 20:25:57 +0800

    适配7.0的通知栏快捷开关 调整ui上的两个小细节 发布2.4.0版本

---
+++ app/src/main/java/com/forfan/bigbang/quicksetting/GlobalCopyTile.java
@@ -0,0 +1,34 @@
+package com.forfan.bigbang.quicksetting;
+
+import android.content.Intent;
+import android.service.quicksettings.TileService;
+
+import com.forfan.bigbang.component.activity.UniversalCopyActivity;
+
+/**
+ * Created by wangyan-pd on 2017/1/12.
+ */
+
+public class GlobalCopyTile extends TileService {
+    private final int STATE_OFF = 0;
+    private final int STATE_ON = 1;
+    private int toggleState = STATE_ON;
+
+    @Override
+    public void onStartListening() {
+        super.onStartListening();
+    }
+
+    @Override
+    public void onTileAdded() {
+        super.onTileAdded();
+    }
+
+    @Override
+    public void onClick() {
+        Intent intent = new Intent();
+        intent.setClass(this, UniversalCopyActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivityAndCollapse(intent);
+    }
+}
